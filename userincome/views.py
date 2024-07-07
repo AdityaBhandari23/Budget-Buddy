f30874729fac3f29b73f0374a6a6cfa943de3377
Aditya <gopalaaditya5@gmail.com>
2024-07-07 12:34:53 +0530

    Income Summary Graph

--- userincome/views.py
+++ userincome/views.py
@@ -9,6 +9,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 import json
 from django.http import JsonResponse
+
+import datetime
 # Create your views here.
 
 
@@ -111,3 +113,34 @@ def delete_income(request, id):
     income.delete()
     messages.success(request, 'record removed')
     return redirect('income')
+
+
+def income_source_summary(request):
+    todays_date=datetime.date.today()
+    six_months_ago=todays_date - datetime.timedelta(days=30*6)
+    income=UserIncome.objects.filter(owner=request.user,
+        date__gte=six_months_ago, date__lte=todays_date
+    )
+    finalrep={}
+
+    def get_source(income):
+        return income.source
+
+    category_list=list(set(map(get_source,income)))
+
+    def get_income_source_amount(source):
+        amount=0
+        filtered_by_source=income.filter(source=source)
+
+        for item in filtered_by_source:
+            amount += item.amount
+        return amount
+
+    for x in income:
+        for y in category_list:
+            finalrep[y]=get_income_source_amount(y)
+
+    return JsonResponse({'income_source_data':finalrep},safe=False)
+
+def income_stats_view(request):
+    return render(request, 'income/stats.html')
